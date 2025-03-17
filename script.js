c72a090a9873277da439d4aee74dab45da702574
Alexandre <bomberdxfull@gmail.com>
2025-03-17 12:07:38 -0300

    Adicionados grids (por hora place holders) ao clicar no botão.   Os grids aparecem ordenados um aoós o outro, também com animações de fade-in

--- script.js
+++ script.js
@@ -2,9 +2,18 @@
 
 const startButton = document.querySelector('.get-started');
 const body = document.querySelector('body');
+const gridContainer = document.querySelector('.grid-container');
+const itemContainers = document.querySelectorAll('.item-container');
 
 startButton.addEventListener('click', () => {
-    body.classList.remove('blur');
+    gridContainer.style.display = 'grid';
+    startButton.style.display = 'none';
+    itemContainers.forEach((container, index) => {
+        container.style.display = 'flex';
+        container.style.animationDelay = `${index * 0.1}s`;
+        //loop para adicionar a classe fade-in a cada container, o template literal garente que cada container tenha um delay diferente
+        container.classList.add('fade-in');
+    });
 });
 
 
